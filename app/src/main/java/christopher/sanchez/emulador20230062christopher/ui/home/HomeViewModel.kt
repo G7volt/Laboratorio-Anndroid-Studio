d9e9baf312d12df8b854700bc3df126fad32396b
Christopher Sanchez <diegsanchez27@gmail.com>
2024-05-02 00:04:04 -0600

    Version 0.1 - Menu login creado

---
+++ app/src/main/java/christopher/sanchez/emulador20230062christopher/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package christopher.sanchez.emulador20230062christopher.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
---
+++ app/src/main/java/christopher/sanchez/emulador20230062christopher/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package christopher.sanchez.emulador20230062christopher.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
